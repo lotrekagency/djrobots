2614b2d1979e33a89eb29837c30970f9f2273f95
Andrea Stagi <stagi.andrea@gmail.com>
2017-10-10 12:38:04 +0200

    Improve templates

--- README.md
+++ README.md
@@ -8,6 +8,12 @@ Include djrobot urls to your URLconf and your're done!
 
     (r'^robots\.txt$', include('djrobots.urls')),
 
+## Sitemap
+
+If you want to specify the sitemap view in your robots.txt, provide the view name in the settings
+
+    DJROBOTS_SITEMAP_VIEW_NAME = 'sitemap-root'
+
 ## Run tests
 
     $ pip install -r requirements-dev.txt
--- djrobots/views.py
+++ djrobots/views.py
@@ -1,9 +1,15 @@
 from django.conf import settings
 from django.shortcuts import render
+from django.urls import reverse
 
 
 # Create your views here.
 def robotstxt(request):
+    sitemap_view_name = getattr(settings, "DJROBOTS_SITEMAP_VIEW_NAME", None)
+    sitemap_url = None
+    if sitemap_view_name:
+        sitemap_url = reverse(sitemap_view_name)
+
     if settings.DEBUG:
         return render(request, 'djrobots/robots-debug.txt', content_type='text/plain')
     else:
