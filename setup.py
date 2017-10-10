6655a3aea3e63232b7f8009f94eb6a5eb2373f6a
Andrea Stagi <stagi.andrea@gmail.com>
2017-10-10 17:07:28 +0200

    Version 0.0.4

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='djrobots',
-    version='0.0.1',
-    url='https://github.com/lotrekagency/cattp',
+    version='0.0.4',
+    url='https://github.com/lotrekagency/djrobots',
     install_requires=[],
     description="djrobots",
     long_description=open('README.rst', 'r').read(),
@@ -11,6 +11,7 @@ setup(
     author="Lotrek",
     author_email="dimmitutto@lotrek.it",
     packages=find_packages(),
+    include_package_data=True,
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
