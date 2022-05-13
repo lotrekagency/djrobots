106387e09a9a1ff64c86ab17d67dadb9669fb92d
bnznamco <gabriele.baldi.01@gmail.com>
2022-05-13 18:17:25 +0200

    test: fix tests to run with django 4

--- tests/urls.py
+++ tests/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import url, include
+from django.urls import re_path, include
 
 def dummy_sitemap(request):
     from django.http import HttpResponse
     return HttpResponse("Here's the text of the Web page.")
 
 urlpatterns = [
-    url(r'^robots\.txt$', include('djrobots.urls')),
-    url(r'^sitemap\.xml$', dummy_sitemap, name='sitemap')
+    re_path(r'^robots\.txt$', include('djrobots.urls')),
+    re_path(r'^sitemap\.xml$', dummy_sitemap, name='sitemap')
 ]
